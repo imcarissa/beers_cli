beed209dcf410751936cf14697ee3b7580387cb8
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-10-02 00:52:27 +0000

    bridge of death

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,7 +1,18 @@
 class CLI 
   
-  def initialize
-    
+  def menu
+    puts "                 Bears, Beers, Battlestar Galactica"
+    puts ""
+    puts ""
+    puts "                  __________________________________"
+    puts "" 
+    puts "          Beer Pairing App according to your Nerd Alter Ego"
+    puts ""
+    puts "              .=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=."
+    puts ""
+    puts "                        First must answer me" 
+    puts "                        These questions three" 
+    puts "                     Ere the other side s/he see. "
   end
   
 end
