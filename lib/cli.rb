b23ec7d84e462658b6c79509d9d5b60cb52e130a
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-10-03 02:14:54 +0000

    FINSIHED

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,27 +9,30 @@ class CLI
     puts ""
     API.get_beers
     print_beers
-    puts ""
-    prompt
-    input = gets.strip
+    input = ""
     while input != "exit" do
-      if input.to_i > 0 && input.to_i <= 24
-        
-      elsif input 
+      puts ""
+      puts "Enter a number from the list to learn more about it or type 'exit' to exit"
+      puts ""
+      input = gets.strip
+        if input.to_i > 0 && input.to_i <= 24
+          puts Beer.all[input.to_i-1].name
+          puts Beer.all[input.to_i-1].abv
+          puts Beer.all[input.to_i-1].tagline
+          puts Beer.all[input.to_i-1].description
+          puts Beer.all[input.to_i-1].food_pairing
+          puts ""
+        elsif
+          puts "Go home, you're drunk. Enter a number from the list"
+        end
     end
   end
 
-
-  def prompt
-    puts ""
-    puts "Enter a number from the list to learn more about it or type 'exit' to exit"
-    puts ""
-  end
 
   def print_beers
     Beer.all.each.with_index(1) do |beer, index|
       puts "#{index}. #{beer.name}"
     end
+    puts ""
   end
-  
 end
