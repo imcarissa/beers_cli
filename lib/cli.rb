0b202e597f45a15d12f1d75bc32106c511a0498a
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-10-04 05:03:45 +0000

    code changes 6

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,12 +1,11 @@
 class CLI 
   
   def menu
-    puts "____~*~__________________~*~_____________________~*~____"
+    puts "   ____~*~____________~*~____________~*~____"
     puts ""
-    puts "                Beer and Food Pairing"
-    puts "              With the Brewdog Catalog"
-    puts "____~*~__________________________________________~*~____"
-    puts ""  
+    puts "Beer and Food Pairing With the Brewdog Catalog"
+    puts "   ____~*~___________________________~*~____"
+    puts "" 
     API.get_beers
     print_beers
     puts ""
@@ -15,19 +14,18 @@ class CLI
     puts ""
     input = ""
     while input != "exit"
-        if input.to_i > 0 && input.to_i <= Beer.all.length
-          beer = Beer.all[input.to_i-1]
-          print_beer_info(info)
+      if input.to_i > 0 && input.to_i <= Beer.all.length
+        beer = Beer.all[input.to_i-1]
         #  puts Beer.all[input.to_i-1].name
        #   puts Beer.all[input.to_i-1].abv
        #   puts Beer.all[input.to_i-1].tagline
        #   puts Beer.all[input.to_i-1].description
        #   puts Beer.all[input.to_i-1].food_pairing
        #   puts ""
-        elsif
-          puts "          Go home buddy, you're drunk."
-        end
+      elsif
+         puts "          Go home buddy, you're drunk."
       end
+    end
   end
 
 
@@ -35,15 +33,6 @@ class CLI
     Beer.all.each.with_index(1) do |beer, index|
       puts "#{index}. #{beer.name}"
     end
-  end
-  
-  
-  def print_beer_info(info)
-    puts "Name: #{beer.name}"
-    puts "ABV: #{beer.abv}"
-    puts "Tagling: #{beer.tagline}"
-    puts "Description: #{beer.description}"
-    puts "Food Pairing Suggestion: #{beer.food_pairing}"
   end
 
 end
