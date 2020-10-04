1f5ce514d59538d15c37bb583578a7b538841bca
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-10-04 04:50:44 +0000

    changes to output

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,24 +9,23 @@ class CLI
     puts ""  
     API.get_beers
     print_beers
-    input = ""
-    while input != "exit" do
     puts ""
     puts "  Enter a number from the selection to find out more"
     puts "  Or type 'exit' to exit"
     puts ""
-    input = gets.strip.downcase
+    input = ""
+    while input != "exit"
         if input.to_i > 0 && input.to_i <= Beer.all.length
-          puts Beer.all[input.to_i-1].name
-          puts Beer.all[input.to_i-1].abv
-          puts Beer.all[input.to_i-1].tagline
-          puts Beer.all[input.to_i-1].description
-          puts Beer.all[input.to_i-1].food_pairing
-          puts ""
+          beer = Beer.all[input.to_i-1]
+          print_beer_info(info)
+        #  puts Beer.all[input.to_i-1].name
+       #   puts Beer.all[input.to_i-1].abv
+       #   puts Beer.all[input.to_i-1].tagline
+       #   puts Beer.all[input.to_i-1].description
+       #   puts Beer.all[input.to_i-1].food_pairing
+       #   puts ""
         elsif
           puts "          Go home buddy, you're drunk."
-        else
-          "Bye!"
         end
       end
   end
@@ -37,4 +36,14 @@ class CLI
       puts "#{index}. #{beer.name}"
     end
   end
+  
+  
+  def print_beer_info(info)
+    puts "Name: #{beer.name}"
+    puts "ABV: #{beer.abv}"
+    puts "Tagling: #{beer.tagline}"
+    puts "Description: #{beer.description}"
+    puts "Food Pairing Suggestion: #{beer.food_pairing}"
+  end
+
 end
