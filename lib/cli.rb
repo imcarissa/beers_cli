c9199109e5b70800561f95b7744b9627cd1cf6d8
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-10-04 04:37:56 +0000

    changes 3

--- lib/cli.rb
+++ lib/cli.rb
@@ -7,14 +7,14 @@ class CLI
     puts "              With the Brewdog Catalog"
     puts "____~*~__________________________________________~*~____"
     puts ""  
+    API.get_beers
+    print_beers
+    input = ""
+    while input != "exit" do
+    puts ""
     puts "  Enter a number from the selection below to find out more or 'exit' to exit"
     puts ""
-    @input = gets.strip.downcase
-    API.get_beers(@input)
-    print_beers
-    prompt
-    input = ""
-      while input != "exit" do
+    input = gets.strip.downcase
         if input.to_i > 0 && input.to_i <= Beer.all.length
           puts Beer.all[input.to_i-1].name
           puts Beer.all[input.to_i-1].abv
@@ -26,14 +26,8 @@ class CLI
           puts "          Go home buddy, you're drunk."
         end
       end
-    end
   end
 
-    def prompt
-      puts ""
-      puts "Type in a number from our list or type 'exit' to exit"
-      puts ""
-    end
 
   def print_beers
     Beer.all.each.with_index(1) do |beer, index|
