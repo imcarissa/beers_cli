664009c5abbf554309d4a89f057f99ae902064a0
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-10-06 20:14:22 +0000

    changes

--- lib/api.rb
+++ lib/api.rb
@@ -3,9 +3,8 @@ class API
   def self.get_beers
     url = "https://api.punkapi.com/v2/beers"
     uri = URI(url)
-    response = Net::HTTP.get(uri) 
-        # making the get resquest to the server, and we get back a string
-        # parsing response string into a readable json object
+    response = Net::HTTP.get(uri) # making the get resquest to the server, and we get back a string
+                                  # parsing response string into a readable json object
     beers = JSON.parse(response)
     beers.each do |b| # iterates through an object array
       Beer.new(name: b["name"], abv: b["abv"], tagline: b["tagline"], description: b["description"], food_pairing: b["food_pairing"]) if b["name"] != ""
